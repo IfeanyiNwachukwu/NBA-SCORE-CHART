58968f937bdd315a75e4880a564766404cbe2a46
Ifeanyi Nwachukwu <ifenwachukwu7@gmail.com>
2021-05-07 03:44:34 +0100

    committing changes

--- app.js
+++ app.js
@@ -83,3 +83,30 @@ const warriorsGames = [{
     }
   }
 ]
+
+const ulParent = document.createElement('ul');
+
+for(let game of warriorsGames){
+    const gameLi = document.createElement('li');
+    const {homeTeam,awayTeam} = game;  // warriors could be the home team or away team
+    const {team :hTeam, points:hPoints} = homeTeam;
+    const {team :aTeam, points:aPoints} = awayTeam;
+    const teamNames = `${aTeam} @ ${hTeam}`;
+    
+    let scoreLine;
+    if(aPoints > hPoints){
+         scoreLine = `<b>${aPoints}</b> - ${hPoints}`;
+    }
+    else{
+         scoreLine = `${aPoints} - <b>${hPoints}</b>`;
+    }
+
+    const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;  // Just saying if homeTeam name is 'Golden State' then warriors are the hometeam, if not they are the away team
+    const className = warriors.isWinner ? 'win' : 'loss';
+    gameLi.classList.add(className);
+
+    gameLi.innerHTML = `${teamNames} ${scoreLine}`;
+    ulParent.appendChild(gameLi);
+}
+
+document.body.prepend(ulParent);
